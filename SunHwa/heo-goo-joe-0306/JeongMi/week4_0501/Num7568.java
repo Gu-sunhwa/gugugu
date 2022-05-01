4a6b4cf61fa3925ab8832b593ab28590f28bc7ad
Gu-sunhwa <102344608+Gu-sunhwa@users.noreply.github.com>
2022-05-01 11:56:21 +0900

    Add files via upload

---
+++ SunHwa/heo-goo-joe-0306/JeongMi/week4_0501/Num7568.java
@@ -0,0 +1,35 @@
+// 덩치(7568)
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Num7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int[] weight = new int[N];
+        int[] height = new int[N];
+        int[] rank = new int[N];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            height[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0;  i < N; i++) {
+            rank[i] = 1;
+            for(int j = 0; j < N; j++) {
+                if(weight[i] < weight[j] && height[i] < height[j]) {
+                    rank[i]++;
+                }
+            }
+        }
+
+        StringBuilder printSet = new StringBuilder();
+        for(int i = 0; i < N; i++)
+            printSet.append(rank[i]).append(" ");
+        
+        System.out.print(printSet);
+    }
+}
